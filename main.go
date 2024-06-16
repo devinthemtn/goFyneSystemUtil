2b6ec493e4efc5275eb4114421088d2a3b9ff698
Thomas Koefod <devinthemtn@gmail.com>
2024-06-15 22:54:13 -0600

    updates text

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
-	"image/color"
+	"os/exec"
 )
 
 func main() {
@@ -15,18 +18,28 @@ func main() {
 	w := a.NewWindow("Main window")
 
 	w.SetContent(widget.NewLabel("Hello World!"))
+	factText := widget.NewLabel("test123")
 	button := widget.NewButton("Speed test", func() {
 		// =====> do some stuff <==== //
+		fmt.Println("I was pressed")
+
+		cmd := exec.Command("ls", "-l")
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Println("Error executing command:", err)
+			return
+		}
+		fmt.Println(string(output))
+		factText.SetText(string(output))
 	})
 
-	title := canvas.NewText("Get Your Useless Facts", color.White)
+	title := canvas.NewText("System Utils", color.White)
 	title.TextStyle = fyne.TextStyle{
 		Bold: true,
 	}
 	title.Alignment = fyne.TextAlignCenter
 	title.TextSize = 24
 
-	factText := widget.NewLabel("test123")
 	hBox := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), button, layout.NewSpacer())
 	vBox := container.New(layout.NewVBoxLayout(), title, hBox, widget.NewSeparator(), factText)
 
