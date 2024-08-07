34aafb25bcccbc9b2cd23ae54d53fad7a3d173b0
Thomas Koefod <devinthemtn@gmail.com>
2024-08-07 09:45:05 -0600

    trying to capture intermediate output as cmd is running

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	// "os"
 	"os/exec"
 )
 
@@ -18,12 +19,14 @@ func main() {
 	w := a.NewWindow("Main window")
 
 	w.SetContent(widget.NewLabel("Hello World!"))
-	factText := widget.NewLabel("test123")
-	button := widget.NewButton("Speed test", func() {
+	factText := widget.NewLabel("")
+	button2 := widget.NewButton("Speed Test", func() {
 		// =====> do some stuff <==== //
 		fmt.Println("I was pressed")
-
-		cmd := exec.Command("ls", "-l")
+		factText.SetText("running speedtest")
+		cmd := exec.Command("speedtest")
+		// cmd.Stdout = os.Stdout
+		// cmd.Stderr = os.Stderr
 		output, err := cmd.Output()
 		if err != nil {
 			fmt.Println("Error executing command:", err)
@@ -31,6 +34,31 @@ func main() {
 		}
 		fmt.Println(string(output))
 		factText.SetText(string(output))
+	})
+	button := widget.NewButton("Test", func() {
+		// =====> do some stuff <==== //
+		fmt.Println("I was pressed")
+		cmd := exec.Command("ls", "-l")
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Println("Error executing command:", err)
+			return
+		}
+		// fmt.Println(string(output))
+		factText.SetText(string(output))
+	})
+	button3 := widget.NewButton("Test", func() {
+		// =====> do some stuff <==== //
+		fmt.Println("I was pressed")
+		factText.SetText("running speedtest")
+		// cmd := exec.Command("ls", "-l")
+		// output, err := cmd.Output()
+		// if err != nil {
+		// 	fmt.Println("Error executing command:", err)
+		// 	return
+		// }
+		// // fmt.Println(string(output))
+		// factText.SetText(string(output))
 	})
 
 	title := canvas.NewText("System Utils", color.White)
@@ -41,7 +69,9 @@ func main() {
 	title.TextSize = 24
 
 	hBox := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), button, layout.NewSpacer())
-	vBox := container.New(layout.NewVBoxLayout(), title, hBox, widget.NewSeparator(), factText)
+	hBox2 := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), button2, layout.NewSpacer())
+	hBox3 := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), button3, layout.NewSpacer())
+	vBox := container.New(layout.NewVBoxLayout(), title, hBox, hBox2, hBox3, widget.NewSeparator(), factText)
 
 	w.SetContent(vBox)
 	w.Show()
